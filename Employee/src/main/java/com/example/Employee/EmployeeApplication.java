f66afe9390c5a36b64ff94b0706429746ceba964
RakiiRakii <rakeshkumar.softsuave@gmail.com>
2023-01-02 15:38:29 +0530

    Develop chick

--- Employee/src/main/java/com/example/Employee/EmployeeApplication.java
+++ Employee/src/main/java/com/example/Employee/EmployeeApplication.java
@@ -1,6 +1,6 @@
 package com.example.Employee;
 /*
-Devlopment works
+Development works develop
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
