596ef0cec5f9fc00a20373ee55cdb4f27918e953
RakiiRakii <rakeshkumar.softsuave@gmail.com>
2023-01-02 15:24:05 +0530

    Package import missing

--- Employee/src/main/java/com/example/Employee/controller/EmployeeController.java
+++ Employee/src/main/java/com/example/Employee/controller/EmployeeController.java
@@ -4,6 +4,7 @@ package com.example.Employee.controller;
 import java.util.List;
 import java.util.Map;
 
+import com.example.Employee.model.Employees;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
